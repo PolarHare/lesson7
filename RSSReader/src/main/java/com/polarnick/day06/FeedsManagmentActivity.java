afb93a04cd575d9ccfc6bb979407408ec2fd8aca
Polarniy Nickolay <PolarHare@gmail.com>
2013-10-30 16:24:09 +0400

    Usability of adding feed greatly improved! (And code improved a bit) At first, user must enter URL, than, if feed by entered URL is not supported - user will be able to correct URL. When URL will point to supported RSS feed, user will be asked to enter feed name(with RSS feed title as default value).

--- RSSReader/src/main/java/com/polarnick/day06/FeedsManagmentActivity.java
+++ RSSReader/src/main/java/com/polarnick/day06/FeedsManagmentActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 import com.polarnick.rss.Feed;
 
 import java.util.ArrayList;
@@ -33,62 +32,8 @@ public class FeedsManagmentActivity extends ListActivity {
         addFeedButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final FeedsSQLiteOpenHelper.FeedEntry newFeed = new FeedsSQLiteOpenHelper.FeedEntry(null, null, false);
-                AlertDialog.Builder dialogName = new AlertDialog.Builder(FeedsManagmentActivity.this);
-                dialogName.setMessage(getResources().getString(R.string.ENTER_FEED_NAME));
-                final EditText input = new EditText(FeedsManagmentActivity.this);
-                dialogName.setView(input);
-                dialogName.setPositiveButton(getResources().getString(R.string.ALERT_OK), new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        newFeed.setName(((TextView) input).getText().toString());
-                        if (newFeed.getName().length() == 0) {
-                            Toast toast = Toast.makeText(FeedsManagmentActivity.this,
-                                    getResources().getString(R.string.FEED_NAME_MUST_NOY_BE_EMPTY), Toast.LENGTH_LONG);
-                            toast.show();
-                        } else if (newFeed.getName().equals(getResources().getString(R.string.MANAGE_FEEDS))) {
-                            Toast toast = Toast.makeText(FeedsManagmentActivity.this,
-                                    "\"" + getResources().getString(R.string.MANAGE_FEEDS) + "\" "
-                                            + getResources().getString(R.string.MUST_NOT_BE_A_FEED_NAME), Toast.LENGTH_LONG);
-                            toast.show();
-                        } else {
-                            AlertDialog.Builder dialogUrl = new AlertDialog.Builder(FeedsManagmentActivity.this);
-                            dialogUrl.setMessage(getResources().getString(R.string.ENTER_FEED_URL));
-                            final EditText input = new EditText(FeedsManagmentActivity.this);
-                            dialogUrl.setView(input);
-                            dialogUrl.setPositiveButton(getResources().getString(R.string.ALERT_OK), new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int whichButton) {
-                                    String url = ((TextView) input).getText().toString();
-                                    String httpPrefix = "http://";
-                                    if (!url.startsWith(httpPrefix)) {
-                                        url = httpPrefix + url;
-                                    }
-                                    newFeed.setUrl(url);
-                                    if (newFeed.getUrl().length() == 0) {
-                                        Toast toast = Toast.makeText(FeedsManagmentActivity.this,
-                                                getResources().getString(R.string.FEED_URL_MUST_NOY_BE_EMPTY), Toast.LENGTH_LONG);
-                                        toast.show();
-                                    } else {
-                                        checkAndAddFeed(newFeed);
-                                    }
-                                }
-                            });
-
-                            dialogUrl.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int whichButton) {
-                                    newFeed.setUrl(null);
-                                }
-                            });
-                            dialogUrl.show();
-                        }
-                    }
-                });
-
-                dialogName.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        newFeed.setName(null);
-                    }
-                });
-                dialogName.show();
+                final FeedsSQLiteOpenHelper.FeedEntry newFeed = new FeedsSQLiteOpenHelper.FeedEntry("", "", false);
+                askUserForFeedURLAndSoOn(newFeed, null);
             }
         });
         sqlHelper = FeedsSQLiteOpenHelper.getInstance(this);
@@ -97,7 +42,54 @@ public class FeedsManagmentActivity extends ListActivity {
         setListAdapter(adapter);
     }
 
-    private void checkAndAddFeed(final FeedsSQLiteOpenHelper.FeedEntry newFeed) {
+    /**
+     * Will ask user for url, validate it, if it is invalid - re-ask url(with default value - previous entered(invalid)),
+     * when url will pass validation - it will ask user to edit feed name(with default value equals to title, retrieved
+     * from RSS feed), than validate it, and when validation will be passed - it will add new feed.
+     *
+     * @param reason can be null or empty. It is added to dialog, as a reason for asking URL. (Normally it can be
+     *               "Entered URL is invalid!")
+     */
+    private void askUserForFeedURLAndSoOn(final FeedsSQLiteOpenHelper.FeedEntry newFeed, String reason) {
+        AlertDialog.Builder dialogUrl = new AlertDialog.Builder(this);
+        if (reason != null && !reason.isEmpty()) {
+            dialogUrl.setMessage(reason + "\n" + getResources().getString(R.string.ENTER_FEED_URL));
+        } else {
+            dialogUrl.setMessage(getResources().getString(R.string.ENTER_FEED_URL));
+        }
+        final EditText input = new EditText(this);
+        input.setText(newFeed.getUrl());
+        dialogUrl.setView(input);
+        dialogUrl.setPositiveButton(getResources().getString(R.string.ALERT_OK), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                String url = ((TextView) input).getText().toString();
+                String httpPrefix = "http://";
+                if (!url.startsWith(httpPrefix)) {
+                    url = httpPrefix + url;
+                }
+                newFeed.setUrl(url);
+                if (newFeed.getUrl().isEmpty()) {
+                    askUserForFeedURLAndSoOn(newFeed, getResources().getString(R.string.FEED_URL_MUST_NOY_BE_EMPTY));
+                } else {
+                    checkURLAndSoOn(newFeed);
+                }
+            }
+        });
+
+        dialogUrl.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                newFeed.setUrl(null);
+            }
+        });
+        dialogUrl.show();
+    }
+
+    /**
+     * Will validate URL, setted in given newFeed. If it is invalid - user will be asked to correct this url.
+     * If it is valid - user will be asked to edit feed name(with default value equals to title, retrieved
+     * from RSS feed), than validate it, and when validation will be passed - it will add new feed.
+     */
+    private void checkURLAndSoOn(final FeedsSQLiteOpenHelper.FeedEntry newFeed) {
         final ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage(getResources().getString(R.string.CHECKING_FEED));
         progressDialog.setCancelable(false);
@@ -106,20 +98,57 @@ public class FeedsManagmentActivity extends ListActivity {
         new FeedDownloadTask() {
             @Override
             public void onSuccess(Feed feed) {
-                sqlHelper.insertEntry(newFeed);
-                feeds.add(newFeed);
-                adapter.notifyDataSetChanged();
+                newFeed.setName(feed.getTitle());
                 progressDialog.dismiss();
+                askUserForFeedNameAndSaveNewFeed(newFeed, null);
             }
 
             @Override
             public void onFailure(Exception exception) {
                 progressDialog.dismiss();
-                Toast toast = Toast.makeText(FeedsManagmentActivity.this,
-                        getResources().getString(R.string.THIS_FEED_IS_NOT_SUPPORTED), Toast.LENGTH_SHORT);
-                toast.show();
+                askUserForFeedURLAndSoOn(newFeed, getResources().getString(R.string.THIS_FEED_IS_NOT_SUPPORTED));
             }
         }.execute(newFeed.getUrl());
+    }
+
+    private void askUserForFeedNameAndSaveNewFeed(final FeedsSQLiteOpenHelper.FeedEntry newFeed, String reason) {
+        AlertDialog.Builder dialogName = new AlertDialog.Builder(this);
+        if (reason != null && !reason.isEmpty()) {
+            dialogName.setMessage(reason + "\n" + getResources().getString(R.string.ENTER_FEED_NAME));
+        } else {
+            dialogName.setMessage(getResources().getString(R.string.ENTER_FEED_NAME));
+        }
+        final EditText input = new EditText(this);
+        input.setText(newFeed.getName());
+        dialogName.setView(input);
+        dialogName.setPositiveButton(getResources().getString(R.string.ALERT_OK), new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                newFeed.setName(((TextView) input).getText().toString());
+                if (newFeed.getName().length() == 0) {
+                    askUserForFeedNameAndSaveNewFeed(newFeed, getResources().getString(R.string.FEED_NAME_MUST_NOY_BE_EMPTY));
+                } else if (newFeed.getName().equals(getResources().getString(R.string.MANAGE_FEEDS))) {
+                    askUserForFeedNameAndSaveNewFeed(newFeed, "\"" + getResources().getString(R.string.MANAGE_FEEDS) + "\" "
+                            + getResources().getString(R.string.MUST_NOT_BE_A_FEED_NAME));
+                } else {
+                    for (FeedsSQLiteOpenHelper.FeedEntry feed : feeds) {
+                        if (feed.getName().equals(newFeed.getName())) {
+                            askUserForFeedNameAndSaveNewFeed(newFeed, getResources().getString(R.string.FEED_NAME_ALREADY_USED));
+                            return;
+                        }
+                    }
+                    sqlHelper.insertEntry(newFeed);
+                    feeds.add(newFeed);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+
+        dialogName.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                newFeed.setName(null);
+            }
+        });
+        dialogName.show();
     }
 
 }
